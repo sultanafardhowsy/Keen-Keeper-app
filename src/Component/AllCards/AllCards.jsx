84790e83f840664b9dcdb7ff11221ca4e0bc6615
sultanafardhowsy <sfardhowsy@gmail.com>
2026-04-16 21:13:20 +0600

    loading added

--- src/Component/AllCards/AllCards.jsx
+++ src/Component/AllCards/AllCards.jsx
@@ -1,27 +1,37 @@
-import React, { use } from 'react';
+import React, { useEffect, useState } from 'react';
 import FriendCard from './CardDetails';
-
-const freindsPromise = fetch('/freinds.json').then(res => res.json())
+import { FadeLoader } from 'react-spinners';
 
 const AllCards = () => {
-   
-         const freinds = use(freindsPromise);
- 
+  const [freinds, setFreinds] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    return (
-        <div className='container mx-auto'>
-          <h2 className='font-bold text-4xl pt-6 mb-10'> Your Freinds</h2>
-          <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-  
-            {
-            freinds.map((friend,ind) =>{
-                return  <FriendCard friend={friend} key={ind}/>
-              })
-          }  
-          </div>
+  useEffect(() => {
+    fetch('/freinds.json')
+      .then(res => res.json())
+      .then(data => {
+        setFreinds(data);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className='container mx-auto'>
+      <h2 className='font-bold text-4xl pt-6 mb-10'>Your Friends</h2>
+
+      {loading ? (
+        <div className="flex justify-center items-center">
+          <FadeLoader />
         </div>
-    
-    );
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {freinds.map((friend, ind) => (
+            <FriendCard friend={friend} key={ind} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default AllCards;
--- src/Pages/FreindDetails/FreindDetails.jsx
+++ src/Pages/FreindDetails/FreindDetails.jsx
@@ -8,215 +8,215 @@ import text from "../../assets/text.png"
 import vedio from "../../assets/video.png"
 import { useContext } from "react";
 import { FreindContext } from "../../Context/freindContext";
- 
+
 
 const FriendDetails = () => {
-  
-  
-  const { id} = useParams();
-  
- const friends = useLoaderData();
- 
+
+
+  const { id } = useParams();
+
+  const friends = useLoaderData();
+
   const expectedFriend = friends.find((friend) => friend.id == id)
- 
- const {name,picture,days_since_contact,goal,bio,status,email,tags,next_due_date} = expectedFriend;
- const statusStyles = {
-  "overdue": "bg-red-500 text-white",
-  "on-track": "bg-green-500 text-white",
-  "almost due": "bg-yellow-500 text-white",
-};
 
-const formatDate = (dateStr) => {
-  if (!dateStr) return "Invalid date";
+  const { name, picture, days_since_contact, goal, bio, status, email, tags, next_due_date } = expectedFriend;
+  const statusStyles = {
+    "overdue": "bg-red-500 text-white",
+    "on-track": "bg-green-500 text-white",
+    "almost due": "bg-yellow-500 text-white",
+  };
 
-  const date = new Date(dateStr);
+  const formatDate = (dateStr) => {
+    if (!dateStr) return "Invalid date";
 
-  if (isNaN(date)) return "Invalid date";
+    const date = new Date(dateStr);
 
-  return date.toLocaleDateString("en-US", {
-    month: "long",   // April
-    day: "2-digit",  // 02
-    year: "numeric", // 2026
-  });
-};
-const { handleCall, handleText, handleVideo } = useContext(FreindContext);
+    if (isNaN(date)) return "Invalid date";
+
+    return date.toLocaleDateString("en-US", {
+      month: "long",   // April
+      day: "2-digit",  // 02
+      year: "numeric", // 2026
+    });
+  };
+  const { handleCall, handleText, handleVideo } = useContext(FreindContext);
 
 
   return (
     <div className="container mx-auto flex justify-between mt-4">
+
       <div className="card bg-base-100 w-96 shadow-sm">
-  <figure className='p-6'>
-    <img
-      src={picture}
-      alt="" className="w-20 h-20 mx-auto rounded-full object-cover"/>
-  </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="text-3xl font-bold">{name}</h2>
-   
-    <div className="mt-4">
-  <span
-    className={`text-xs px-3 py-1 rounded-full ${
-      statusStyles[status.toLowerCase()] || "bg-gray-100 text-gray-600"
-    }`}
-  >
-    {status.charAt(0).toUpperCase() + status.slice(1)}
-  </span>
-    <div className="flex justify-center gap-2 mt-3 flex-wrap">
-        {tags.map((tag, index) => (
-          <span
-            key={index}
-            className="text-xs bg-green-100 text-green-700 px-2 py-1 rounded-full"
-          >
-            {tag.toUpperCase()}
-          </span>
-        ))}
+        <figure className='p-6'>
+          <img
+            src={picture}
+            alt="" className="w-20 h-20 mx-auto rounded-full object-cover" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="text-3xl font-bold">{name}</h2>
+
+          <div className="mt-4">
+            <span
+              className={`text-xs px-3 py-1 rounded-full ${statusStyles[status.toLowerCase()] || "bg-gray-100 text-gray-600"
+                }`}
+            >
+              {status.charAt(0).toUpperCase() + status.slice(1)}
+            </span>
+            <div className="flex justify-center gap-2 mt-3 flex-wrap">
+              {tags.map((tag, index) => (
+                <span
+                  key={index}
+                  className="text-xs bg-green-100 text-green-700 px-2 py-1 rounded-full"
+                >
+                  {tag.toUpperCase()}
+                </span>
+              ))}
+            </div>
+          </div>
+          <p className="font-bold">"{bio}"</p>
+          <p>Preferred: {email}</p>
+        </div>
+        <button className="btn btn-xl mt-5"><RiNotificationSnoozeLine />Snooze 2 weeks</button>
+        <button className="btn btn-xl mt-5"><FaBoxArchive />Archive</button>
+        <button className="btn btn-xl mt-5"><img src={deleteIcon} alt="" className="w-6 h-6" /> Delete</button>
       </div>
-</div>
-<p className="font-bold">"{bio}"</p>
-    <p>Preferred: {email}</p>
-  </div>
-  <button className="btn btn-xl mt-5"><RiNotificationSnoozeLine />Snooze 2 weeks</button>
-  <button className="btn btn-xl mt-5"><FaBoxArchive />Archive</button>
-  <button className="btn btn-xl mt-5"><img src={deleteIcon} alt="" className="w-6 h-6"/> Delete</button>
-</div>
-      
-   <div className="ml-5">
-    <div className="flex justify-between gap-5 mt-10">
-     <div>
-      <div className="card w-86 bg-base-100 card-sm shadow-sm">
-  <div className="card-body items-center py-10">
-    <h2 className="text-4xl font-bold">{days_since_contact}</h2>
-    <p className="font-bold">Days Since Contact</p>
-    
-  </div>
-</div>
 
-    </div>
-     <div>
-      <div className="card w-86 bg-base-100 card-sm shadow-sm">
-  <div className="card-body items-center py-10">
-    <h2 className="text-4xl font-bold">{goal}</h2>
-    <p className="font-bold">Goal ( DAYS )</p>
-    
-  </div>
-</div>
+      <div className="ml-5">
+        <div className="flex justify-between gap-5 mt-10">
+          <div>
+            <div className="card w-86 bg-base-100 card-sm shadow-sm">
+              <div className="card-body items-center py-10">
+                <h2 className="text-4xl font-bold">{days_since_contact}</h2>
+                <p className="font-bold">Days Since Contact</p>
 
-    </div>
-    <div className="grid grid-cols-1">
-     <div className="">
-      <div className="card w-86 bg-base-100 card-sm shadow-sm">
-  <div className="card-body items-center py-10">
-    <h2 className="card-title text-3xl font-bold">{formatDate(next_due_date)}</h2>
-    <p className="font-bold">Next Due</p>
-    
-  </div>
-</div>
+              </div>
+            </div>
 
-    </div>
-   </div>
-   </div>
-   <div className="card card-border bg-base-100 ">
-  <div className="card-body grid grid-cols-2 w-296">
-    <div className="">
-      <h2 className="card-title text-2xl font-bold">Relationship Goal</h2>
-    <p className="font-bold">Connect every <span className="font-bold text-xl">{goal} days</span></p>
-    </div>
-    <div className="card-actions justify-end">
-      <button className="btn btn-nutral">Edit</button>
-    </div>
-  </div>
-</div>
-<div className="grid grid-cols-3">
-  <div>
-      <div className="card w-86 bg-base-100 card-sm shadow-sm py-10">
-  <button
-  onClick={() => handleCall(expectedFriend)}
-  className="card-body items-center w-full text-center cursor-pointer hover:bg-gray-100 transition rounded-xl"
->
-  <figure>
-    <img src={call} alt="" className="w-6 h-6" />
-  </figure>
-  <p className="font-bold">Call</p>
-</button>
-</div>
+          </div>
+          <div>
+            <div className="card w-86 bg-base-100 card-sm shadow-sm">
+              <div className="card-body items-center py-10">
+                <h2 className="text-4xl font-bold">{goal}</h2>
+                <p className="font-bold">Goal ( DAYS )</p>
 
-    </div>
-      <div className="">
-      <div className="card w-86 bg-base-100 card-sm shadow-sm py-10">
-  <button
-  onClick={() => handleText(expectedFriend)}
-  className="card-body items-center w-full text-center cursor-pointer hover:bg-gray-100 transition rounded-xl"
->
-  <figure>
-    <img src={text} alt="" className="w-6 h-6" />
-  </figure>
-  <p className="font-bold">Text</p>
-</button>
-</div>
+              </div>
+            </div>
 
-    </div>
-      <div>
-      <div className="card w-86 bg-base-100 card-sm shadow-sm py-10">
-  <button
-  onClick={() => handleVideo(expectedFriend)}
-  className="card-body items-center w-full text-center cursor-pointer hover:bg-gray-100 transition rounded-xl"
->
-  <figure>
-    <img src={vedio} alt="" className="w-6 h-6" />
-  </figure>
-  <p className="font-bold">Video</p>
-</button>
-</div>
+          </div>
+          <div className="grid grid-cols-1">
+            <div className="">
+              <div className="card w-86 bg-base-100 card-sm shadow-sm">
+                <div className="card-body items-center py-10">
+                  <h2 className="card-title text-3xl font-bold">{formatDate(next_due_date)}</h2>
+                  <p className="font-bold">Next Due</p>
 
-    </div>  
-</div>
-<div className="container mx-auto bg-base-100 card-sm shadow-sm ">
-  <div className="flex justify-between mt-10 ">
-    <h2 className="text-xl font-bold">Recent Interactions</h2>
-    <div className="flex justify-between gap-1">
-      <figure className=''>
-    <img
-      src={vedio}
-      alt="" className="w-6 h-6"/>
-  </figure>
-  <p className="font-bold">Full History</p>
-    </div>
-  </div>
-  <div className="flex justify-between mt-10">
-    <div>
-      <figure className=''>
-    <img
-      src={call}
-      alt="" className="w-6 h-6"/>
-  </figure>
-    <p className="font-bold">Call</p>
-    </div>
-    <p className="font-bold">Feb 26, 2026</p>
-  </div>
-  <div className="flex justify-between mt-10">
-    <div>
-      <figure className=''>
-    <img
-      src={vedio}
-      alt="" className="w-6 h-6"/>
-  </figure>
-    <p className="font-bold">Vedio</p>
-    </div>
-    <p className="font-bold">Feb 26, 2026</p>
-  </div>
-  <div className="flex justify-between mt-10">
-    <div>
-      <figure className=''>
-    <img
-      src={text}
-      alt="" className="w-6 h-6"/>
-  </figure>
-    <p className="font-bold">Text</p>
-    </div>
-    <p className="font-bold">Feb 26, 2026</p>
-  </div>
-</div>
-    </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div className="card card-border bg-base-100 ">
+          <div className="card-body grid grid-cols-2 w-296">
+            <div className="">
+              <h2 className="card-title text-2xl font-bold">Relationship Goal</h2>
+              <p className="font-bold">Connect every <span className="font-bold text-xl">{goal} days</span></p>
+            </div>
+            <div className="card-actions justify-end">
+              <button className="btn btn-nutral">Edit</button>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-3">
+          <div>
+            <div className="card w-86 bg-base-100 card-sm shadow-sm py-10">
+              <button
+                onClick={() => handleCall(expectedFriend)}
+                className="card-body items-center w-full text-center cursor-pointer hover:bg-gray-100 transition rounded-xl"
+              >
+                <figure>
+                  <img src={call} alt="" className="w-6 h-6" />
+                </figure>
+                <p className="font-bold">Call</p>
+              </button>
+            </div>
+
+          </div>
+          <div className="">
+            <div className="card w-86 bg-base-100 card-sm shadow-sm py-10">
+              <button
+                onClick={() => handleText(expectedFriend)}
+                className="card-body items-center w-full text-center cursor-pointer hover:bg-gray-100 transition rounded-xl"
+              >
+                <figure>
+                  <img src={text} alt="" className="w-6 h-6" />
+                </figure>
+                <p className="font-bold">Text</p>
+              </button>
+            </div>
+
+          </div>
+          <div>
+            <div className="card w-86 bg-base-100 card-sm shadow-sm py-10">
+              <button
+                onClick={() => handleVideo(expectedFriend)}
+                className="card-body items-center w-full text-center cursor-pointer hover:bg-gray-100 transition rounded-xl"
+              >
+                <figure>
+                  <img src={vedio} alt="" className="w-6 h-6" />
+                </figure>
+                <p className="font-bold">Video</p>
+              </button>
+            </div>
+
+          </div>
+        </div>
+        <div className="container mx-auto bg-base-100 card-sm shadow-sm ">
+          <div className="flex justify-between mt-10 ">
+            <h2 className="text-xl font-bold">Recent Interactions</h2>
+            <div className="flex justify-between gap-1">
+              <figure className=''>
+                <img
+                  src={vedio}
+                  alt="" className="w-6 h-6" />
+              </figure>
+              <p className="font-bold">Full History</p>
+            </div>
+          </div>
+          <div className="flex justify-between mt-10">
+            <div>
+              <figure className=''>
+                <img
+                  src={call}
+                  alt="" className="w-6 h-6" />
+              </figure>
+              <p className="font-bold">Call</p>
+            </div>
+            <p className="font-bold">Feb 26, 2026</p>
+          </div>
+          <div className="flex justify-between mt-10">
+            <div>
+              <figure className=''>
+                <img
+                  src={vedio}
+                  alt="" className="w-6 h-6" />
+              </figure>
+              <p className="font-bold">Vedio</p>
+            </div>
+            <p className="font-bold">Feb 26, 2026</p>
+          </div>
+          <div className="flex justify-between mt-10">
+            <div>
+              <figure className=''>
+                <img
+                  src={text}
+                  alt="" className="w-6 h-6" />
+              </figure>
+              <p className="font-bold">Text</p>
+            </div>
+            <p className="font-bold">Feb 26, 2026</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
